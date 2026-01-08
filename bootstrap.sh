dda28b27d6c1a3b4746305ccbfbaf4fa417882d8
alvaroroobles <alvaro.becerril.robles@gmail.com>
2026-01-08 19:27:19 +0100

    Agregamos instalación del admin-area y copiamos el archivo del contexto

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,14 @@ apt-get update
 
 apt-get -y upgrade
 
-apt-get -y install openjdk-11-jdk tomcat9
+apt-get -y install openjdk-11-jdk tomcat9 tomcat9-admin
 
 groupadd -f tomcat9
 
-useradd -s /bin/false -g tomcat9 -d /etc/tomcat9 tomcat9
+id -u tomcat9 >/dev/null 2>&1 || useradd -s /bin/false -g tomcat9 -d /etc/tomcat9 tomcat9
+
+cp /vagrant/context.xml /usr/share/tomcat9-admin/host-manager/META-INF/context.xml
+
+systemctl restart tomcat9
+
+systemctl status tomcat9
