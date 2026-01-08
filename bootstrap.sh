3610fb523324b2549bbdabf9a05a526478a8bc72
alvaroroobles <alvaro.becerril.robles@gmail.com>
2026-01-08 19:45:16 +0100

    Introducimos gestion de usuarios / GUI

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,14 +6,16 @@ apt-get update
 
 apt-get -y upgrade
 
-apt-get -y install openjdk-11-jdk tomcat9 tomcat9-admin
+apt-get -y install openjdk-11-jdk tomcat9 tomcat9-admin maven
 
 groupadd -f tomcat9
 
 id -u tomcat9 >/dev/null 2>&1 || useradd -s /bin/false -g tomcat9 -d /etc/tomcat9 tomcat9
 
 cp /vagrant/context.xml /usr/share/tomcat9-admin/host-manager/META-INF/context.xml
+cp /vagrant/tomcat-users.xml /etc/tomcat9/tomcat-users.xml
 
 systemctl restart tomcat9
 
 systemctl status tomcat9
+
